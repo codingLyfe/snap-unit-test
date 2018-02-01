edcd34dbf74807dc64ebdaffca91a4054f4e7678
Tristan Bennett <tmbennett2011@msn.com>
2018-02-01 08:40:31 -0700

    attempt at snap Unit Test for Medium Article

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Snap Unit Test</title>
+	</head>
+	<body>
+		<h1>Snap Unit Test</h1>
+		<h3>Why do we write setup methods in database unit tests?</h3>
+		<p>The setup method in database unit testing DELETES all previous data so that the unit test is easily accessible and the information is clear. It then pre defines certain things like "profile" information so that tests in other areas don't have to create that data, just access it.</p>
+	</body>
+</html>
