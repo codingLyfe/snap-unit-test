0b93fe6df5de93e4e728367fa53194292c9f292d
Tristan Bennett <tmbennett2011@msn.com>
2018-02-01 08:59:14 -0700

    updated values

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
 	<body>
 		<h1>Snap Unit Test</h1>
 		<h3>Why do we write setup methods in database unit tests?</h3>
-		<p>The setup method in database unit testing DELETES all previous data so that the unit test is easily accessible and the information is clear. It then pre defines certain things like "profile" information so that tests in other areas don't have to create that data, just access it.</p>
+		<p>The setup method in database unit testing DELETES all previous data so that the unit test is easily accessible and the information is clear. It then pre defines certain things like "profile" information so that tests in other areas don't have to create that data, just access it. </p>
 	</body>
 </html>
--- unitTest.php
+++ unitTest.php
@@ -6,7 +6,7 @@ class foo {
 	public final function setUp(): void {
 		parent::setUp();
 		$password = "abc123";
-		$this->VALID_PROFILE_SALT = bin2hex(random_bytes(32));
+		$this->VALID_PROFILE_SALT = bin2hex(random_bytes(16));
 		$this->VALID_PROFILE_HASH = hash_pbkdf2("sha512", $password, $this->VALID_PROFILE_SALT, 26144);
 
 		$this->profile = new Profile(generateUUidV4(), null, "mark@markymark.io", $this->VALID_PROFILE_HASH, "Mark Jones", $this->VALID_PROFILE_SALT);
